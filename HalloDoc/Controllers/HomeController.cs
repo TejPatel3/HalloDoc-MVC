cbcb472884a1fe0500191219620e438cdfd8f73d
SERVER7\PCE156 <tej.patel@eTatvasoft.com>
2024-02-07 19:17:29 +0530

    Database Integration

--- HalloDoc/Controllers/HomeController.cs
+++ HalloDoc/Controllers/HomeController.cs
@@ -1,29 +1,50 @@
-﻿using HalloDoc.Models;
+﻿using HalloDoc.DataContext;
+using HalloDoc.DataModels;
+using HalloDoc.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
+
 
 namespace HalloDoc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
         public IActionResult Index()
         {
             return View();
+            //return _context.AspNetUsers != null ?
+            //             View(await _context.AspNetUsers.ToListAsync()) :
+            //             Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+
         }
 
         public IActionResult ForgotPassword()
         {
+
             return View();
         }
 
         public IActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(AspNetUser obj)
+        {
+            if (_context.AspNetUsers.Where
+                (m => m.Email == obj.Email).Any() && _context.AspNetUsers.Where(user => user.PasswordHash == obj.PasswordHash).Any())
+            {
+                return RedirectToAction("ForgotPassword");
+            }
+            Console.WriteLine("hellolllllllllllllllllllllllllllllllllllllllllllllllllllll");
             return View();
         }
 
@@ -36,6 +57,6 @@ namespace HalloDoc.Controllers
         }
     }
 
- 
+
 
 }
